556752465c4727853dffa9e62dbfdf5e23b34454
Brayden Rayner <braydenrayner@outlook.com>
2023-11-12 20:31:10 -0400

    Added comments

--- blink.asm
+++ blink.asm
@@ -2,10 +2,12 @@
 .include "m328Pdef.inc"
 
 main:
-	ldi r16, 0xff
-	out DDRB, r16
+	ldi r16, 0xff		
+	out DDRB, r16		; Set all pins in PORTB to OUTPUT
 
 loop:
 	ldi r17, 0xff
-	out PORTB, r17
+	out PORTB, r17		; Write HIGH to all pins on PORTB
+	ldi r17, 0x00
+	out PORTB, r17		; Write LOW to all pins on PORTB
 	rjmp loop
